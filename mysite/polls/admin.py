fc2c053338fe4c69c346e2646f625f2d26797fa4
Rosendo Lopez Serrano <rosendo.lopez.contractor@bbva.com>
2022-10-13 10:51:42 -0500

    primer commit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question, Choice
+
+#admin.site.register(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "status", "order"]
+    list_display_links = ["pub_date"]
+    list_editable = ["status"]
+
+admin.site.register(Choice)
