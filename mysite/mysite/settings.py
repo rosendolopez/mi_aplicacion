4920f054991929ab8095cad9e9ebe53f31606d71
Rosendo Lopez Serrano <rosendo.lopez.contractor@bbva.com>
2022-10-12 23:51:06 -0500

    primer commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django_extensions',
-    'mysite.polls.apps.PollsConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
